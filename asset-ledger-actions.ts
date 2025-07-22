28a23c016d92f5094432072797b7979afb16b1d0
Saransh Khulbe <55129013+saranshkhulbe7@users.noreply.github.com>
2025-07-22 18:02:28 +0530

    Update asset-ledger-actions.ts

--- asset-ledger-actions.ts
+++ asset-ledger-actions.ts
@@ -15,14 +15,19 @@ export const ACTIONS: Record<string, ActionConfig> = {
     display: "Eggs Purchased",
     msg: ({ amount }) => `You bought ${amount} eggs`,
   },
+
+// BREAK EGG TO GEM PROCESS 2 STEPS: START
   breakEggToGem: {
     display: "Eggs broken",
     msg: ({ amount }) => `You broke ${amount} eggs to get gems`,
   },
-  convertedEggFromGem: {
-    display: "Eggs Purchased",
-    msg: ({ amount }) => `You bought ${amount} eggs from gems`,
+  breakedGemFromEgg: {
+    display: "Gems Received",
+    msg: ({ amount }) => `You received ${amount} gems for breaking eggs`,
   },
+  // BREAK EGG TO GEM PROCESS 2 STEPS: END
+
+  
   inNest: {
     display: "Eggs Nested",
     msg: ({ amount, nestName }) =>
@@ -52,14 +57,18 @@ export const ACTIONS: Record<string, ActionConfig> = {
     display: "Sell gems rejected",
     msg: ({ amount }) => `Sale of ${amount} gems failed`,
   },
-  breakedGemFromEgg: {
-    display: "Gems Received",
-    msg: ({ amount }) => `You received ${amount} gems for breaking eggs`,
-  },
+
+  // CONVERT GEM TO EGG PROCESS 2 STEPS: START
   convertGemToEgg: {
     display: "Gems used",
     msg: ({ amount }) => `You used ${amount} gems to buy eggs`,
   },
+  convertedEggFromGem: {
+    display: "Eggs Purchased",
+    msg: ({ amount }) => `You bought ${amount} eggs from gems`,
+  },
+  // CONVERT GEM TO EGG PROCESS 2 STEPS: END
+
   "egging-gem": {
     display: "Gems earned",
     msg: ({ amount, nestName }) =>
@@ -75,6 +84,8 @@ export const ACTIONS: Record<string, ActionConfig> = {
   },
 
   /* Coin-side */
+  
+  // CONVERT COIN TO GEM PROCESS 2 STEPS: START
   convertCoinToGem: {
     display: "Coins used",
     msg: ({ amount }) => `You used ${amount} coins to buy gems`,
@@ -83,6 +94,9 @@ export const ACTIONS: Record<string, ActionConfig> = {
     display: "Gems Purchased",
     msg: ({ amount }) => `You bought ${amount} gems from coins`,
   },
+  // CONVERT COIN TO GEM PROCESS 2 STEPS: END
+
+  // CONVERT COIN TO EGG PROCESS 2 STEPS: START
   convertCoinToEgg: {
     display: "Coins used",
     msg: ({ amount }) => `You used ${amount} coins`,
@@ -91,6 +105,8 @@ export const ACTIONS: Record<string, ActionConfig> = {
     display: "Eggs purchased",
     msg: ({ amount }) => `You bought ${amount} eggs from coins`,
   },
+  // CONVERT COIN TO EGG PROCESS 2 STEPS: END
+  
   "unlock-nest": {
     display: "Nest unlocked",
     msg: ({ nestName }) => `Nest ${nestName} unlocked`,
